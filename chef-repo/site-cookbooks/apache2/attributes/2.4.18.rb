024a1c993bedb366689feaf56969fab6b157ce19
@ontheroad.jp <dev@ontheroad.jp>
2016-01-21 17:52:25 +0900

    [WIP] Jan 21, 17:52

---
+++ chef-repo/site-cookbooks/apache2/attributes/2.4.18.rb
@@ -0,0 +1,5 @@
+default['httpd']['server_admin']    = 'root@localhost'
+default['httpd']['server_name']     = 'www.example.com:80'
+default['httpd']['document_root']   = '/usr/local/apache2/htdocs'
+default['httpd']['docroot_allow_override']    = 'All'
+
---
+++ chef-repo/site-cookbooks/apache2/recipes/2.4.18.rb
@@ -0,0 +1,131 @@
+#
+# Cookbook Name:: apache2
+# Recipe:: default
+#
+# Copyright 2015, @ontheroad_jp
+#
+# All rights reserved - Do Not Redistribute
+#
+user "apache" do
+    shell    '/bin/false'
+    system  true
+    action  :create
+end
+
+group "apache" do
+    append true
+    members 'apache'
+    action :modify
+end
+
+
+# Install Build tools
+%w{gcc make pcre pcre-devel wget}.each do |pkg|
+	package pkg do
+		action [:install, :upgrade]
+		# action :install
+	end
+end
+
+# Install ARP(Apache Portable Runtime): http://apr.apache.org/download.cgi
+remote_file "/usr/local/src/apr-1.5.2.tar.gz" do
+    source 'http://ftp.yz.yamagata-u.ac.jp/pub/network/apache//apr/apr-1.5.2.tar.gz'
+    action :create
+end
+
+execute "source compile apr" do
+    user "root"
+    command <<-EOH
+        cd /usr/local/src
+        tar -xvzf apr-1.5.2.tar.gz
+        cd /usr/local/src/apr-1.5.2
+        ./configure --prefix=/opt/apr/apr-1.5.2
+        make
+        make install
+        EOH
+    action :run
+    only_if { ::File.exists?("/usr/local/src/apr-1.5.2.tar.gz")}
+end
+
+# Install ARP-util: http://apr.apache.org/download.cgi
+remote_file "/usr/local/src/apr-util-1.5.4.tar.gz" do
+    source 'http://ftp.yz.yamagata-u.ac.jp/pub/network/apache//apr/apr-util-1.5.4.tar.gz'
+    action :create
+end
+
+execute "source compile apr-util" do
+    user "root"
+    command <<-EOH
+        cd /usr/local/src
+        tar -xvzf apr-util-1.5.4.tar.gz
+        cd /usr/local/src/apr-util-1.5.4
+        ./configure --prefix=/opt/apr/apr-util-1.5.4 --with-apr=/opt/apr/apr-1.5.2
+        make
+        make install
+        EOH
+    action :run
+    only_if { ::File.exists?("/usr/local/src/apr-util-1.5.4.tar.gz")}
+end
+
+# Install Apache: http://apr.apache.org/download.cgi
+remote_file "/usr/local/src/httpd-2.4.18.tar.gz" do
+    source 'http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.18.tar.gz'
+    action :create
+end
+
+execute "source compile Apache" do
+    user "root"
+    command <<-EOH
+        cd /usr/local/src
+        tar -xvzf httpd-2.4.18.tar.gz
+        cd httpd-2.4.18
+        ./configure \
+        --with-apr=/opt/apr/apr-1.5.2 \
+        --with-apr-util=/opt/apr/apr-util-1.5.4
+        make
+        make install
+        EOH
+    action :run
+    only_if { ::File.exists?("/usr/local/src/httpd-2.4.18.tar.gz")}
+end
+
+# Change owner
+execute "Change directory owner to apache" do
+    user "root"
+    command <<-EOH
+        cd /usr/local
+        chown -R apache:apache apache2/
+        EOH
+    action :run
+    only_if { ::File.exists?("/usr/local/apache2")}
+end
+
+
+# Replace httpd.conf
+template "/usr/local/apache2/conf/httpd.conf" do
+  source "2.4.18/httpd.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+# Add service script
+template "/etc/rc.d/init.d/httpd" do
+  source "2.4.18/httpd.init.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+# Regist service
+execute "regist service" do
+    user "root"
+    command <<-EOH
+    chkconfig --add httpd
+    EOH
+    action :run
+end
+
+
+
+
