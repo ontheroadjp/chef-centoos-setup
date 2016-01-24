28b5ca964b2bf4c747b222b0e29980c344e83143
@ontheroad.jp <dev@ontheroad.jp>
2016-01-24 13:30:18 +0900

    [Change] default value of 'DocumentRoot' to '/usr/local/apache2/htdocs'

--- chef-repo/site-cookbooks/apache2/attributes/2.4.18.rb
+++
@@ -1,5 +0,0 @@
-default['httpd']['server_admin']    = 'root@localhost'
-default['httpd']['server_name']     = 'www.example.com:80'
-default['httpd']['document_root']   = '/usr/local/apache2/htdocs'
-default['httpd']['docroot_allow_override']    = 'All'
-
