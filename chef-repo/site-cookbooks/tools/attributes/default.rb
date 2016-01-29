411ab9772694cb967b925b3e90a0ebdc91f92621
@ontheroad.jp <dev@ontheroad.jp>
2016-01-29 10:20:26 +0900

    [Replace] installing 'gettext' recipe from 'php56/recipes/5.6.17.rb' to 'tools/recepes/default.rb'

--- chef-repo/site-cookbooks/tools/attributes/default.rb
+++ chef-repo/site-cookbooks/tools/attributes/default.rb
@@ -1,6 +1,7 @@
 
 default['tools']['git'] = true
 default['tools']['ctags'] = true
+default['tools']['gettext'] = true
 default['tools']['htop'] = true
 default['tools']['vim'] = false
 default['tools']['vim-lua'] = true
