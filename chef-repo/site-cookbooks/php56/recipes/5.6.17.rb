e9d2828bdaf7bc85212e23c779421386ba8bcfbd
@ontheroad.jp <dev@ontheroad.jp>
2016-02-02 21:39:04 +0900

    [Remove] openssl & cURL

--- chef-repo/site-cookbooks/php56/recipes/5.6.17.rb
+++ chef-repo/site-cookbooks/php56/recipes/5.6.17.rb
@@ -29,56 +29,6 @@ end
 end
 
 # -------------------------------------
-# Install cURL Source code
-# -------------------------------------
-package "libssh2-devel" do
-	action [:install, :upgrade]
-end
-remote_file "/usr/local/src/curl-7.47.0.tar.gz" do
-    source 'http://curl.haxx.se/download/curl-7.47.0.tar.gz'
-    not_if { ::File.exists?("/usr/local/src/curl-7.47.0.tar.gz")}
-    action :create
-end
-execute "Un tarball" do
-    cwd "/usr/local/src"
-    user "root"
-    command "tar -xvzf curl-7.47.0.tar.gz"
-    action :run
-    only_if { ::File.exists?("/usr/local/src/curl-7.47.0.tar.gz")}
-    not_if { ::File.exists?("/usr/local/src/curl-7.47.0")}
-end
-execute "cURL ./configure" do
-    cwd "/usr/local/src/curl-7.47.0"
-    user "root"
-    environment(
-        "USE_CCACHE" => "1",
-        "CCACHE_DIR" => "/root/.ccache",
-        "CC" => "ccache gcc",
-        "CXX" => "ccache g++"
-    )
-    command "./configure --prefix=/usr/local --with-ssl=/usr/local/openssl --with-libssh2"
-    action :run
-end
-execute "cURL make" do
-    cwd "/usr/local/src/curl-7.47.0"
-    user "root"
-    environment(
-        "USE_CCACHE" => "1",
-        "CCACHE_DIR" => "/root/.ccache",
-        "CC" => "ccache gcc",
-        "CXX" => "ccache g++"
-    )
-    command "make -j 4"
-    action :run
-end
-execute "cURL make install" do
-    cwd "/usr/local/src/curl-7.47.0"
-    user "root"
-    command "make -j 4 install"
-    action :run
-end
-
-# -------------------------------------
 # Install PHP Source code
 # -------------------------------------
 remote_file "/usr/local/src/php-5.6.17.tar.gz" do
