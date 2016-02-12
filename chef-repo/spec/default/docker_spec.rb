c8413f1f04f428b4466ffeb909894e3bf0932028
@ontheroad.jp <dev@ontheroad.jp>
2016-02-12 10:16:28 +0900

    [New] spec file for docker recipe

---
+++ chef-repo/spec/default/docker_spec.rb
@@ -0,0 +1,21 @@
+
+# Create user:docker
+describe user('docker') do
+    it { should exist }
+    it { should belong_to_group 'docker' }
+end
+
+# Create group:docker
+describe group('docker') do
+    it { should exist }
+end
+
+# Install Build tools
+packages = ['docker-io']
+packages.each do |pkg|
+    describe package("#{pkg}") do
+        it { should be_installed }
+    end
+end
+
+
