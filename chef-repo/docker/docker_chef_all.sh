f1490a3a33b0adcafb6ae457647a72391829b5fb
@ontheroad.jp <dev@ontheroad.jp>
2016-02-16 17:04:50 +0900

    [Add] comment for sample src

--- chef-repo/docker/docker_chef_all.sh
+++ chef-repo/docker/docker_chef_all.sh
@@ -2,8 +2,11 @@
 
 . docker/conf.txt
 
+CHEFHOME=../
+
 # Chef の実行
 #knife solo bootstrap root@${ipaddress} --defaults -i /var/lib/jenkins/.ssh/id_rsa
+# knife solo bootstrap root@${ipaddress} -c ${CHEFHOME}/.chef/knife.rb -j ${CHEFHOME}/nodes/${ipaddress}.json --defaults || RET=$?
 knife solo bootstrap root@${ipaddress} --defaults || RET=$?
 
 exit $RET
