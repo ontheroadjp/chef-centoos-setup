c25c66b8d96ddb034f40cd3f624d3869ec7842d5
@ontheroad_jp <dev@ontheroad.jp>
2016-02-24 11:37:03 +0900

    [Fix] "should match /4:off/" to "should match /4:on/"

--- chef-repo/spec/testcase/webmin_spec.rb
+++ chef-repo/spec/testcase/webmin_spec.rb
@@ -31,7 +31,7 @@ describe command('chkconfig --list webmin') do
     its(:stdout) { should match /1:off/ }
     its(:stdout) { should match /2:on/ }
     its(:stdout) { should match /3:on/ }
-    its(:stdout) { should match /4:off/ }
+    its(:stdout) { should match /4:on/ }
     its(:stdout) { should match /5:on/ }
     its(:stdout) { should match /6:off/ }
 end
