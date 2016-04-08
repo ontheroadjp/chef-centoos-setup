d62ec5eb76169318f70bf458aaa7027713cd687d
@ontheroad_jp <dev@ontheroad.jp>
2016-04-08 23:49:08 +0900

    [Modify] add if statement for OS junction

--- chef-repo/site-cookbooks/ntpd/recipes/default.rb
+++ chef-repo/site-cookbooks/ntpd/recipes/default.rb
@@ -7,21 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# install ntp
-package 'ntp' do
-    action [:install, :upgrade]
-end
-template "/etc/ntp.conf" do
-  source "ntp.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
 
-# start ntp
-service "ntpd" do
-    action [:start, :enable]
-    supports :status => true, :restart => true, :reload => true
-    only_if {node['service']['ntp']}
-end
+if platform_family?('rhel') && node['platform_version'].to_i == 6 then
+    # install ntp
+    package 'ntp' do
+        action [:install, :upgrade]
+    end
+    template "/etc/ntp.conf" do
+      source "ntp.conf.erb"
+      owner "root"
+      group "root"
+      mode 0644
+    end
+    
+    # start ntp
+    service "ntpd" do
+        action [:start, :enable]
+        supports :status => true, :restart => true, :reload => true
+        only_if {node['service']['ntp']}
+    end
 
+elsif platform_family?('rhel') && node['platform_version'].to_i == 7 then
+    # use chronyd insted of ntpd for CentOS7
+end
