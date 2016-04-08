5585962a47140c8107dda8ea72c5eab933bc23d9
@ontheroad_jp <dev@ontheroad.jp>
2016-04-09 00:14:34 +0900

    [Modify] add if statement for OS junction

--- chef-repo/site-cookbooks/iptables/recipes/default.rb
+++ chef-repo/site-cookbooks/iptables/recipes/default.rb
@@ -7,33 +7,43 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# settings for iptables
-template "/root/iptables_rules_setting.sh" do
-  source "iptables_rules_setting.sh.erb"
-  owner "root"
-  group "root"
-  mode 0700
-end
+if platform_family?('rhel') && node['platform_version'].to_i == 6 then
 
-execute "ipconfig_rules_setting" do
-    user "root"
-	command "sh /root/iptables_rules_setting.sh"
-end
+    # settings for iptables
+    template "/root/iptables_rules_setting.sh" do
+      source "iptables_rules_setting.sh.erb"
+      owner "root"
+      group "root"
+      mode 0700
+    end
+    
+    execute "ipconfig_rules_setting" do
+        user "root"
+    	command "sh /root/iptables_rules_setting.sh"
+    end
+    
+    # settings for rsyslog 
+    template "/etc/rsyslog.conf" do
+      source "rsyslog.conf.erb"
+      owner "root"
+      group "root"
+      mode 0755
+    end
+    
+    # settings for logrotate
+    template "/etc/logrotate.d/iptables" do
+      source "iptables_logrotate.erb"
+      owner "root"
+      group "root"
+      mode 0644
+    end
 
-# settings for rsyslog 
-template "/etc/rsyslog.conf" do
-  source "rsyslog.conf.erb"
-  owner "root"
-  group "root"
-  mode 0755
-end
-
-# settings for logrotate
-template "/etc/logrotate.d/iptables" do
-  source "iptables_logrotate.erb"
-  owner "root"
-  group "root"
-  mode 0644
+elsif platform_family?('rhel') && node['platform_version'].to_i == 7 then
+    execute "regist service" do
+        user "root"
+        command "systemctl enable httpd.service"
+        action :run
+    end
 end
 
 
