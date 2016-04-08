aac372865b2d83465cfa8dc26b79debfa525f875
@ontheroad_jp <dev@ontheroad.jp>
2016-04-09 00:01:27 +0900

    [Modify] reading nodes/xxx.json file

--- chef-repo/spec/testcase/service_spec.rb
+++ chef-repo/spec/testcase/service_spec.rb
@@ -2,7 +2,32 @@ require 'serverspec'
 
 # Start apache
 # Start MySQL
-services = ['httpd','mysqld']
+
+
+services = []
+if property['service']['httpd'] == true
+    services.push('httpd')
+end
+if property['service']['mysqld'] == true
+    services.push('mysqld')
+end
+if property['service']['webmin'] == true
+    services.push('webmin')
+end
+if property['service']['usermin'] == true
+    services.push('usermin')
+end
+if property['service']['jenkins'] == true
+    services.push('jenkins')
+end
+if property['service']['docker'] == true
+    services.push('docker')
+end
+if property['service']['ntp'] == true
+    services.push('ntp')
+end
+
+#services = ['httpd','mysqld']
 services.each do | service |
     describe service("#{service}") do
         it { should be_enabled }
